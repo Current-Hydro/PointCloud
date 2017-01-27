d3cdc84a1d0dd590cc64a0a1d68ee5558f0438a2
Jason Zisheng Chang <chang.j@live.com>
2017-01-26 23:59:08 -0500

    Setup framework for Parse Database Storage
    Backend setup

---
+++ DataLoggingApp/Pods/Target Support Files/Pods-Hydroelectric AppTests/Pods-Hydroelectric AppTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Hydroelectric_AppTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Hydroelectric_AppTestsVersionString[];
+
